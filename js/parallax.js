9b5aebf0c16ca61cc2e8e1c120725f8c20cfa7fa
Dmitry Marinov <marinovdf@gmail.com>
2013-06-18 23:56:06 +0300

    верстака до подгонки

--- js/parallax.js
+++ js/parallax.js
@@ -116,10 +116,10 @@ $(document).ready(function () {
         if ($bg3.hasClass("inview")) {bg3.css({'backgroundPosition': newPos(50, windowHeight, pos, 200, 0.5)});}
         if ($bg4.hasClass("inview")) {bg4.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.7)});}
         if ($bg5.hasClass("inview")) {bg5.css({'backgroundPosition': newPos(50, windowHeight, pos, -1000, 0.04)});}
-        if ($bg6.hasClass("inview")) {bg6.css({'backgroundPosition': newPos(50, windowHeight, pos, 100, 0.1)});}
-        if ($bg7.hasClass("inview")) {bg7.css({'backgroundPosition': newPos(50, windowHeight, pos, 0, 0.5)});}
+        if ($bg6.hasClass("inview")) {bg6.css({'backgroundPosition': newPos(50, windowHeight, pos, 3500, 0.1)});}
+        if ($bg7.hasClass("inview")) {bg7.css({'backgroundPosition': newPos(50, windowHeight, pos, -1500, 0.5)});}
         if ($bg8.hasClass("inview")) {bg8.css({'backgroundPosition': newPos(50, windowHeight, pos, -1500, 0.3)});}
-        if ($bg9.hasClass("inview")) {bg9.css({'backgroundPosition': newPos(50, windowHeight, pos, 700, 0.2)});} // TODO add new rules if needed
+        if ($bg9.hasClass("inview")) {bg9.css({'backgroundPosition': newPos(50, windowHeight, pos,2500, 0.2)});} // TODO add new rules if needed
     }
 
     //MOBILE OPTIMIZED PARALLAX EXECUTION
